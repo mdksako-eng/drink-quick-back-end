a5dcca2009f8458e9c44c2bb2f623fbe77d1977a
mdksako-eng <mdksako@gmail.com>
2026-05-04 22:50:45 +0100

    Refactor email service and add reset code email

--- utils/email.service.js
+++ utils/email.service.js
@@ -18,7 +18,7 @@ const createTransporter = () => {
     return nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
-        secure: process.env.EMAIL_PORT === 465,
+        secure: parseInt(process.env.EMAIL_PORT) === 465,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
